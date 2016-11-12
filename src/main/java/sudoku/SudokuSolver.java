89016b93062822d1b462ebdff1b5c906126bbc43
goldsmithwill <goldsmith.will@gmail.com>
2016-11-12 11:13:34 -0500

    Finished solver, validator, and just created GUI class.

--- src/main/java/sudoku/SudokuSolver.java
+++ src/main/java/sudoku/SudokuSolver.java
@@ -14,23 +14,57 @@ public class SudokuSolver {
 				{ 3, 0, 0, 0, 9, 0, 0, 0, 0 } };
 
 		// checking if board can be solved
-		System.out.println(SudokuValidator.isValid(board));
+		// System.out.println(SudokuValidator.isValid(board));
+		System.out.println(solve(board));
 
 	}
 
-	public static int[][] solve(int[][] solution) {
-
-		for (int i = 0; i < solution.length; i++) {
-
+	public static boolean solve(int[][] solution) {
+		// nested for loops go through sudoku board
+		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
+				// if the square is empty
+				if (solution[i][j] == 0) {
+					for (int k = 1; k <= 9; k++) {
+						// putting in values 1-9 in the square
+						solution[i][j] = k;
+						/*
+						 * if the board with the new value is valid and if it's
+						 * able to be solved (it goes on a crazy recursive thing
+						 * that backtracks at the end)
+						 */
+						if (SudokuValidator.isValid(solution) && solve(solution)) {
+							// printing the solved board
+							// (but only after it's fully complete)
+							if (i == 0 && j == 0) {
+								printBoard(solution);
+							}
+							return true;
+						}
 
-				
-				
+						// if the 1-9 value doesn't work, reset square to 0
+						solution[i][j] = 0;
+					}
+					/*
+					 * it returns false if the values that were put in earlier
+					 * were wrong, helping with the recursive
+					 */
+					return false;
+				}
 			}
-
 		}
+		return true;
+	}
 
-		return solution;
+	public static void printBoard(int[][] board) {
+		// System.out.println("\n\n\n");
+		for (int i = 0; i < board.length; i++) {
+			System.out.println("");
+			for (int j = 0; j < 9; j++) {
+				System.out.print(board[i][j] + "  ");
+
+			}
+		}
 	}
 
 }
